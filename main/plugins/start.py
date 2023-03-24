f52385d1b8ff2fb3a69945fceef7c1716e9297b6
Mr. Invisible <122687482+mrinvisible7@users.noreply.github.com>
2023-03-24 12:08:22 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,20 +1,20 @@
 #Github.com/mrinvisible7
 
 import os
-from .. import bot as Drone
+from .. import bot as Invix
 from telethon import events, Button
 
 #from ethon.mystarts import start_srb
     
 S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
-@Drone.on(events.callbackquery.CallbackQuery(data="set"))
+@Invix.on(events.callbackquery.CallbackQuery(data="set"))
 async def sett(event):    
-    Drone = event.client
+    Invix = event.client
     button = await event.get_message()
     msg = await button.get_reply_message()
     await event.delete()
-    async with Drone.conversation(event.chat_id) as conv: 
+    async with Invix.conversation(event.chat_id) as conv: 
         xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
         x = await conv.get_reply()
         if not x.media:
@@ -30,9 +30,9 @@ async def sett(event):
         os.rename(path, f'./{event.sender_id}.jpg')
         await t.edit("Temporary thumbnail saved!")
         
-@Drone.on(events.callbackquery.CallbackQuery(data="rem"))
+@Invix.on(events.callbackquery.CallbackQuery(data="rem"))
 async def remt(event):  
-    Drone = event.client            
+    Invix = event.client            
     await event.edit('Trying.')
     try:
         os.remove(f'{event.sender_id}.jpg')
@@ -40,10 +40,10 @@ async def remt(event):
     except Exception:
         await event.edit("No thumbnail saved.")                        
   
-@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
+@Invix.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
     text = "👋 Hi, I am 'Save Restricted Content ' bot.\n\n✅ Send me the Link of any message of Restricted Channels to Clone it here. 
-For private channel's messages, send the Invite Link first.\n\n👨🏻‍💻Owner:  [𝙄𝙉𝙑𝙄𝙓](@mister_invisiblebot).\n**support:** @mr_invisible_bots"
+For private channel's messages, send the Invite Link first.\n\n👨🏻‍💻Owner: @mister_invisiblebot.\n**support:** @mr_invisible_bots"
     #await start_srb(event, text)
     await event.reply(text, 
                       buttons=[
